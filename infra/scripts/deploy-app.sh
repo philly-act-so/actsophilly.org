802c06df2792ec3c13fa690aef335c9fedc6374d
Douglas Naphas <douglasnaphas@gmail.com>
2025-05-26 18:25:49 -0400

    Use us-east-1

--- infra/scripts/deploy-app.sh
+++ infra/scripts/deploy-app.sh
@@ -2,4 +2,4 @@
 
 set -e
 STACKNAME=$(npx @cdk-turnkey/stackname@2.1.0 --suffix actsophilly-org --hash 5);
-AWS_REGION=us-east-2 CDK_DEFAULT_REGION=us-east-2 npx cdk --region=us-east-2 deploy --require-approval never ${STACKNAME};
+AWS_REGION=us-east-1 CDK_DEFAULT_REGION=us-east-1 npx cdk --region=us-east-1 deploy --require-approval never ${STACKNAME};
