67700534485de45756f0dbf6c97dcceb1a430560
Douglas Naphas <douglasnaphas@gmail.com>
2025-05-26 22:27:31 -0400

    Revert "Delete the API to fix "already exists" errors"
    This reverts commit dc5ccd7e59353d7851cb80f79bc8252777d800c0.

--- infra/lib/actsophilly.org-stack.ts
+++ infra/lib/actsophilly.org-stack.ts
@@ -1,72 +1,67 @@
-import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import { Construct } from "constructs";
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { Construct } from 'constructs';
 
 export class ActsophillyOrgStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // Add API Gateway with a regional endpoint
-    // const api = new apigateway.RestApi(this, 'ActsophillyOrgApi', {
-    //   restApiName: 'ActsophillyOrgApi',
-    //   description: 'API Gateway for redirecting to Philly ACT-SO site',
-    //   endpointConfiguration: {
-    //     types: [apigateway.EndpointType.REGIONAL],
-    //   },
-    //   deploy: false, // Disable automatic deployment
-    // });
+    const api = new apigateway.RestApi(this, 'ActsophillyOrgApi', {
+      restApiName: 'ActsophillyOrgApi',
+      description: 'API Gateway for redirecting to Philly ACT-SO site',
+      endpointConfiguration: {
+        types: [apigateway.EndpointType.REGIONAL],
+      },
+      deploy: false, // Disable automatic deployment
+    });
 
-    // // Define a method for the root path "/"
-    // api.root.addMethod(
-    //   'GET',
-    //   new apigateway.MockIntegration({
-    //     integrationResponses: [
-    //       {
-    //         statusCode: '301',
-    //         responseParameters: {
-    //           'method.response.header.Location': `'https://sites.google.com/view/philly-act-so'`,
-    //         },
-    //       },
-    //     ],
-    //     requestTemplates: {
-    //       'application/json': '{"statusCode": 301}',
-    //     },
-    //   }),
-    //   {
-    //     methodResponses: [
-    //       {
-    //         statusCode: '301',
-    //         responseParameters: {
-    //           'method.response.header.Location': true,
-    //         },
-    //       },
-    //     ],
-    //   }
-    // );
+    // Define a method for the root path "/"
+    api.root.addMethod(
+      'GET',
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: '301',
+            responseParameters: {
+              'method.response.header.Location': `'https://sites.google.com/view/philly-act-so'`,
+            },
+          },
+        ],
+        requestTemplates: {
+          'application/json': '{"statusCode": 301}',
+        },
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: '301',
+            responseParameters: {
+              'method.response.header.Location': true,
+            },
+          },
+        ],
+      }
+    );
 
-    // // Create a custom deployment
-    // const deployment = new apigateway.Deployment(this, 'CustomDeployment', {
-    //   api,
-    // });
+    // Create a custom deployment
+    const deployment = new apigateway.Deployment(this, 'CustomDeployment', {
+      api,
+    });
 
-    // // Create a custom stage with no suffix
-    // const stage = new apigateway.Stage(this, 'CustomStage', {
-    //   deployment,
-    //   stageName: '', // Use an empty string to remove the stage name
-    // });
+    // Create a custom stage with no suffix
+    const stage = new apigateway.Stage(this, 'CustomStage', {
+      deployment,
+      stageName: '', // Use an empty string to remove the stage name
+    });
 
-    // // Associate the custom stage with the API
-    // api.deploymentStage = stage;
+    // Associate the custom stage with the API
+    api.deploymentStage = stage;
 
-    // // Add a CloudFormation output for the API Gateway endpoint
-    // new CfnOutput(this, 'ApiGatewayUrl', {
-    //   value: api.url,
-    //   description: 'The URL of the API Gateway endpoint',
-    // });
-    new CfnOutput(this, "NoResources", {
-      value: "This stack does not create any resources.",
-      description:
-        "This stack is a placeholder and does not create any resources.",
+    // Add a CloudFormation output for the API Gateway endpoint
+    new CfnOutput(this, 'ApiGatewayUrl', {
+      value: api.url,
+      description: 'The URL of the API Gateway endpoint',
     });
   }
 }
